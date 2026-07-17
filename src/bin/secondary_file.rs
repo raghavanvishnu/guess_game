b23b5d541a3ad0944e4e8e4f35718f305b70ee8a
raghavanvishnu <raghavan.vmvs@gmail.com>
2026-07-17 10:57:59 +0530

    added new functionality

--- src/bin/secondary_file.rs
+++ src/bin/secondary_file.rs
@@ -11,5 +11,11 @@ fn main() {
     // Program execution starts here.
     // Empty parentheses () mean this function takes no parameters.
     println!("Guess The number");
-    println!("Please input ypur guess")
+    println!("Please input ypur guess");
+
+    let mut guess = String::new();// assigned a mutable variable 
+    // this is a variable of type string
+    //the staring instance is empty
+
+
 }
